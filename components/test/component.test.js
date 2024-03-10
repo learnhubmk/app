66a3c67c1265c09bbf316254c1d6601b8f73f6b7
Dimitar <dimihbt@yahoo.com>
2024-03-10 19:19:47 +0100

    jest setup

--- components/test/component.test.js
+++ components/test/component.test.js
@@ -1,10 +1,3 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import MyComponent from './MyComponent'; 
-
-test('renders MyComponent with correct text', () => {
-  render(<MyComponent />);
-  const textElement = screen.getByText(/Hello, World!/i);
-  expect(textElement).toBeInTheDocument();
+test('Jest Test Example', () => {
+  expect(true).toBe(true);
 });
