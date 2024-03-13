f20b1a73df5d0c35c6b75c683b61d8f50b1460ab
Konstantin Vckov <kvckov@yahoo.com>
2024-03-13 09:31:16 +0100

    s

---
+++ components/cursor-component/Cursor.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Cursor() {
+  return (
+    <div className="cursor">
+      <h1>This is the cursor pointer style</h1>
+    </div>
+  );
+}
+
+export default Cursor;
