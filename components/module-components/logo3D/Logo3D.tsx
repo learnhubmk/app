775b355057a84df908dc8b403a689a0e5053a26f
Viktor <Revan_TSL@hotmail.com>
2024-03-24 20:15:46 +0100

    fixed logo loading, added cool logo

--- components/module-components/logo3D/Logo3D.tsx
+++ components/module-components/logo3D/Logo3D.tsx
@@ -1,20 +1,23 @@
 'use client';
 
-import { Suspense, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
+import { Object3D } from 'three';
 // @ts-ignore
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { Object3D } from 'three';
 import { Canvas } from '@react-three/fiber';
-import { Stats, OrbitControls } from '@react-three/drei';
+import { OrbitControls, Stats } from '@react-three/drei';
 
 const Logo3D = () => {
   const loader = new GLTFLoader();
   // @ts-ignore
   const [models, setModels] = useState<[Object3D | null]>(null);
-  // @ts-ignore
-  loader.load('models/logo3d.glb', async (gltf) => {
-    const loadedModels = await gltf.parser.getDependencies('node');
-    setModels(loadedModels);
+  useEffect(() => {
+    // @ts-ignore
+    loader.load('models/logo3D_cool.glb', async (gltf) => {
+      // TODO: rename variables to to match data being loaded
+      const loadedModels = await gltf.parser.getDependencies('scene');
+      setModels(loadedModels);
+    });
   });
 
   const renderModels =
@@ -22,13 +25,13 @@ const Logo3D = () => {
     models.map((model) => (
       // @ts-ignore
       // eslint-disable-next-line react/no-unknown-property
-      <primitive object={model} />
+      <primitive key={model?.id} object={model} rotation={[0, -Math.PI / 2, 0]} />
     ));
   return (
     <div
       style={{ width: '100%', height: '400px', backgroundColor: '#121d36', marginBottom: '100px' }}
     >
-      <Canvas camera={{ position: [0, 0, -0.8], near: 0.025 }}>
+      <Canvas camera={{ position: [0, -0.3, -3], near: 0.025 }}>
         <Suspense>
           {/* eslint-disable-next-line react/no-unknown-property */}
           <ambientLight intensity={0.2} />
@@ -40,7 +43,8 @@ const Logo3D = () => {
           {/* eslint-disable-next-line react/no-unknown-property */}
           <spotLight intensity={1} position={[0, 0, 0]} castShadow />
         </Suspense>
-        <OrbitControls autoRotate autoRotateSpeed={1} />
+        <OrbitControls autoRotate autoRotateSpeed={5} />
+        {/* <OrbitControls /> */}
         <Stats />
       </Canvas>
     </div>
