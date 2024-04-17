39b159c34ebf4e7668073d065cfc2cb6788da3d6
Miki Vitkovski <mikivitkovski@gmail.com>
2024-04-17 15:30:38 +0200

    #159 Separate two branches

--- components/reusable-components/notification/notification.tsx
+++ components/reusable-components/notification/notification.tsx
@@ -5,25 +5,34 @@ interface NotificationProps {
   message: string;
   type: 'success' | 'error';
   onClose: () => void;
+  duration: number;
 }
 
-const Notification = ({ message, type, onClose }: NotificationProps) => {
+const Notification = ({ message, type, onClose, duration }: NotificationProps) => {
   const [isVisible, setIsVisible] = useState(true);
+  const [timeRemaining, setTimeRemaining] = useState(100);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
+    const intervalId = setInterval(() => {
+      setTimeRemaining((prevTime) => prevTime - 100 / (duration / 1000));
+    }, 800);
+
+    const timeoutId = setTimeout(() => {
       setIsVisible(false);
       onClose();
-    }, 3500);
+    }, duration);
 
-    return () => clearTimeout(timer);
-  }, [onClose]);
+    return () => {
+      clearInterval(intervalId);
+      clearTimeout(timeoutId);
+    };
+  }, [duration, onClose]);
 
   return (
     <div className={styles.popup} style={{ display: isVisible ? 'flex' : 'none' }}>
       <div className={`${styles.notification} ${styles[type]}`}>
-        {type === 'success' && <p className={styles.success}>{message}</p>}
-        {type === 'error' && <p className={styles.error}>{message}</p>}
+        <p className={styles.message}>{message}</p>
+        <div className={styles.timerBar} style={{ width: `${timeRemaining}%` }} />
       </div>
     </div>
   );
