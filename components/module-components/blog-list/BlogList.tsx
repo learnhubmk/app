a4fa09ba7d30a6757f19b8b9397e3e40aec8caeb
kire21 <kire.zdraveski@gmail.com>
2024-04-19 11:52:50 +0200

    #147 - fixed blogs layout

--- components/module-components/blog-list/BlogList.tsx
+++ components/module-components/blog-list/BlogList.tsx
@@ -20,7 +20,9 @@ const BlogList = async ({ pageTitle, gridLayout, blogCardsNumber }: BlogListProp
 
   return (
     <>
-      <div className={`grid ${gridLayout} ${style.blogListContainer}`}>{data}</div>
+      <div className={`grid ${gridLayout} ${pageTitle === 'home' && style.blogListContainer}`}>
+        {data}
+      </div>
 
       {pageTitle === 'home' && (
         <Link href="/blog" className={style.blogBtn}>
@@ -29,7 +31,7 @@ const BlogList = async ({ pageTitle, gridLayout, blogCardsNumber }: BlogListProp
       )}
 
       {pageTitle === 'blog' && (
-        <InfiniteScroll gridLayout={gridLayout} pageTitle={pageTitle} blogCardsNumber={8} />
+        <InfiniteScroll gridLayout={gridLayout} pageTitle={pageTitle} blogCardsNumber={6} />
       )}
     </>
   );
