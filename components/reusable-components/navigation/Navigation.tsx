cfac6bf71fd83ee9e215b2276049ecaa8af643b4
Miki Vitkovski <mikivitkovski@gmail.com>
2024-04-23 21:55:10 +0200

    #173 Update the Navigation with toggle button from new design and delete Sign In and Sign Up buttons

--- components/reusable-components/navigation/Navigation.tsx
+++ components/reusable-components/navigation/Navigation.tsx
@@ -7,21 +7,20 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './navigation.module.scss';
-import Button from '../button/Button';
-import solImage from '../../../public/theme/sol.png';
-import moonImage from '../../../public/theme/lune.png';
 import { useTheme } from '../../../app/context/themeContext';
+import solImage from '../../../public/theme/sun.png';
+import moonImage from '../../../public/theme/moon.png';
 
 const Navigation = () => {
   const { theme, toggleTheme } = useTheme();
-  const isLightTheme = theme === 'light';
+  const isSun = theme === 'light';
 
   return (
     <nav className={styles.largeNavbar}>
       <div className={styles.navContainer}>
         <Link className={`display-s ${styles.navigationLogo}`} href="/">
           <Image
-            src={isLightTheme ? '/logo/logo-white.svg' : '/logo/logo-black.svg'}
+            src={isSun ? '/logo/logo-white.svg' : '/logo/logo-black.svg'}
             className={`${styles.navigationLogo}`}
             alt="LearnHub Logo"
             width={250}
@@ -29,62 +28,35 @@ const Navigation = () => {
           />
         </Link>
 
-        <div className={styles.buttonsContent}>
-          <div>
-            <Link href="/signup" passHref>
-              <Button
-                style={{
-                  backgroundColor: 'var(--bg-color)',
-                  padding: '10px',
-                  marginRight: '10px',
-                  color: '#ea713e',
-                  border: '1px solid #ea713e',
-                  fontSize: '20px',
-                  paddingInline: '25px',
-                }}
-              >
-                Sign Up
-              </Button>
-            </Link>
-
-            <Link href="/signin" passHref>
-              <Button
-                style={{
-                  backgroundColor: '#f9664b',
-                  padding: '10px',
-                  fontSize: '20px',
-                  paddingInline: '25px',
-                }}
-              >
-                Sign In
-              </Button>
-            </Link>
+        <div
+          className={`${styles.themeBackgroundSize} ${!isSun ? styles.themeDark && styles.themeBackgroundLight : styles.themeLight && styles.themeBackgroundDark}`}
+          onClick={toggleTheme}
+        >
+          <div className={`${styles.animate} ${!isSun ? styles.moveRight : ''}`}>
+            {isSun ? (
+              <div className={`${styles.ellipse1} ${styles.ellipse}`} />
+            ) : (
+              <div className={`${styles.ellipse} ${styles.ellipse1}`} />
+            )}
           </div>
 
-          <div
-            className={`${styles.themeBackgroundSize} ${!isLightTheme ? styles.themeDark && styles.themeBackgroundLight : styles.themeLight && styles.themeBackgroundDark}`}
-            onClick={toggleTheme}
-          >
-            <div className={`${styles.animate} ${!isLightTheme ? styles.moveRight : ''}`}>
-              {isLightTheme ? (
-                <Image
-                  className={`${styles.theme_icon}`}
-                  src={solImage}
-                  alt="Sun"
-                  width={25}
-                  height={25}
-                />
-              ) : (
-                <Image
-                  className={`${styles.theme_icon}`}
-                  src={moonImage}
-                  alt="Moon"
-                  width={25}
-                  height={25}
-                />
-              )}
-            </div>
-          </div>
+          {isSun ? (
+            <Image
+              src={moonImage}
+              alt="Sun Image"
+              width={18}
+              height={18}
+              className={styles.customImageClass}
+            />
+          ) : (
+            <Image
+              src={solImage}
+              alt="Moon Image"
+              width={18}
+              height={18}
+              className={styles.customImageClass1}
+            />
+          )}
         </div>
       </div>
     </nav>
