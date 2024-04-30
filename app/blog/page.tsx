3167cf99d1307e8eecce14b036d2c6e8a721a75c
Nino Kadric <ninman27@gmail.com>
2024-04-30 18:59:26 +0200

    added tag filter component

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,16 +1,14 @@
-// 'use client';
+'use client';
 
 import BlogList from '../../components/module-components/blog-list/BlogList';
 import Tab from '../../components/reusable-components/tab/Tab';
-// import { useTagFilterContext } from '../context/tagFilterContext';
-// import AddNewPost from '../addNewPost';
+import { useTagFilterContext } from '../context/tagFilterContext';
 
 const Blog = () => {
-  // const { selectedTags } = useTagFilterContext();
-  // console.log(selectedTags);
+  const { selectedTags } = useTagFilterContext();
+  console.log(selectedTags);
   return (
     <div>
-      {/* <AddNewPost /> */}
       <Tab leftTabText="За тебе" rightTabText="Најново" />
       <BlogList pageTitle="blog" gridLayout="grid__1x3" blogCardsNumber={6} />
     </div>
