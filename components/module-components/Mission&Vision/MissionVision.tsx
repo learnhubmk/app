c18fbbb97ef1b1f95c76633cc5a8aaf0ec540ae7
Micev <micev@outlook.com>
2024-05-01 03:20:48 +0200

    Configuring responsive design for Mission&Vision. This is not final changes

--- components/module-components/Mission&Vision/MissionVision.tsx
+++ components/module-components/Mission&Vision/MissionVision.tsx
@@ -14,7 +14,13 @@ const MissionVision = () => {
       <div
         className={`${style.MissionVisionMain} ${isLightTheme ? style.missionMainWhite : style.missionMainDark}`}
       >
-        <Image src="/mission_vision.png" alt="Our mission & vision" width={1239} height={1031} />
+        <Image
+          className={style.imgMission}
+          src="/mission_vision.png"
+          alt="Our mission & vision"
+          width={1239}
+          height={1031}
+        />
 
         <Image
           className={style.missionPattern}
@@ -27,22 +33,40 @@ const MissionVision = () => {
         <div
           className={`${style.missionTitle} ${isLightTheme ? style.missionTitleWhite : style.missionTitleDark}`}
         >
-          <h2>Мисија</h2>
-          <p>
-            Мисијата на LearnHub е негување на одржлива и инклузивна заедница од сите припадници на
-            IТ-секторот во Македонија, од почетници-ентузијасти до експерти со децениско искуство во
-            струката со цел заедничко спојување на вештините и искуствата во насока на тимска
-            соработка на реални проекти и безрезервна поддршка и едукација во текот на целиот процес
-            од идеја до пазар и одржување.
-          </p>
+          <div className={style.cardMission}>
+            <Image
+              className={style.mobilePattern}
+              src="./frame5.svg"
+              alt="Mobile Pattern"
+              width={109}
+              height={109}
+            />
+            <h2>Мисија</h2>
+            <p>
+              Мисијата на LearnHub е негување на одржлива и инклузивна заедница од сите припадници
+              на IТ-секторот во Македонија, од почетници-ентузијасти до експерти со децениско
+              искуство во струката со цел заедничко спојување на вештините и искуствата во насока на
+              тимска соработка на реални проекти и безрезервна поддршка и едукација во текот на
+              целиот процес од идеја до пазар и одржување.
+            </p>
+          </div>
 
-          <h2>Визија</h2>
-          <p>
-            Визијата на LearnHub е да биде препознатива заедница по соработката, едукацијата и
-            поддршката за сите луѓе кои дејствуваат во IT секторот и пошироко. Заедница од каде што
-            многу нови производи и услуги ќе бидат резултат на заеднички напори и спој на различни
-            вештини од повеќе области во технолошкиот свет.
-          </p>
+          <div className={style.cardMission}>
+            <Image
+              className={style.mobilePattern}
+              src="./frame5.svg"
+              alt="Mobile Pattern"
+              width={109}
+              height={109}
+            />
+            <h2>Визија</h2>
+            <p>
+              Визијата на LearnHub е да биде препознатива заедница по соработката, едукацијата и
+              поддршката за сите луѓе кои дејствуваат во IT секторот и пошироко. Заедница од каде
+              што многу нови производи и услуги ќе бидат резултат на заеднички напори и спој на
+              различни вештини од повеќе области во технолошкиот свет.
+            </p>
+          </div>
         </div>
       </div>
     </div>
