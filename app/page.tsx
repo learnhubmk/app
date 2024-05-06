0361804d6d0a53fda1b479b928968e36c93d2eed
Dimitar <dimihbt@yahoo.com>
2024-05-06 19:38:42 +0200

    fixing and improving the layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
+import React from 'react';
 import FAQ from '../components/module-components/FAQ/FAQ';
 import Hero from '../components/module-components/hero/Hero';
 import BlogList from '../components/module-components/blog-list/BlogList';
 import LearnhubAnimation from '../components/module-components/learnhub-animation/LearnhubAnimation';
-import styles from './page.module.scss';
 import Newsletter from '../components/reusable-components/newsletter/Newsletter';
 import MemberList from '../components/module-components/member-list/MemberList';
 
@@ -11,7 +11,7 @@ import Contact from '../components/module-components/Contact/Contact';
 
 const Home = () => {
   return (
-    <main className={styles.main}>
+    <>
       <Hero
         title="Развијте го вашиот потенцијал"
         text="Овозможете си да достигнете нови височини, развивајќи и споделувајќи вештини cо заедница од ентузијасти и професионалци. Со LearnHub, вие не сте само дел  
@@ -23,7 +23,7 @@ const Home = () => {
       <MemberList />
       <FAQ data={FAQ_DUMMY_DATA} />
       <Contact />
-    </main>
+    </>
   );
 };
 export default Home;
