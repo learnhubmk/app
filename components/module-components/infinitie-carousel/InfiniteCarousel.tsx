01b2c7fed383872415db992db18ccf34aa5172a8
kire21 <kire.zdraveski@gmail.com>
2024-05-09 19:49:16 +0200

    #175 created infinite carousel

---
+++ components/module-components/infinitie-carousel/InfiniteCarousel.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import style from './infiniteCarousel.module.scss';
+
+const InfiniteCarousel = () => {
+  return (
+    <div className={style.slider}>
+      <div className={style.slideTrack}>
+        <div className={style.slide}>
+          <p>UI/UX Design</p>
+        </div>
+        <div className={style.slide}>
+          <p>Frontend Development</p>
+        </div>
+        <div className={style.slide}>
+          <p>Backend Development</p>
+        </div>
+        <div className={style.slide}>
+          <p>Quality Assurance</p>
+        </div>
+        <div className={style.slide}>
+          <p>DevOps</p>
+        </div>
+        <div className={style.slide}>
+          <p>Marketing</p>
+        </div>
+        <div className={style.slide}>
+          <p>Product Management</p>
+        </div>
+        <div className={style.slide}>
+          <p>UI/UX Design</p>
+        </div>
+        <div className={style.slide}>
+          <p>Frontend Development</p>
+        </div>
+        <div className={style.slide}>
+          <p>Backend Development</p>
+        </div>
+        <div className={style.slide}>
+          <p>Quality Assurance</p>
+        </div>
+        <div className={style.slide}>
+          <p>DevOps</p>
+        </div>
+        <div className={style.slide}>
+          <p>Marketing</p>
+        </div>
+        <div className={style.slide}>
+          <p>Product Management</p>
+        </div>
+        <div className={style.slide}>
+          <p>UI/UX Design</p>
+        </div>
+        <div className={style.slide}>
+          <p>Frontend Development</p>
+        </div>
+        <div className={style.slide}>
+          <p>Backend Development</p>
+        </div>
+        <div className={style.slide}>
+          <p>Quality Assurance</p>
+        </div>
+        <div className={style.slide}>
+          <p>DevOps</p>
+        </div>
+        <div className={style.slide}>
+          <p>Marketing</p>
+        </div>
+        <div className={style.slide}>
+          <p>Product Management</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InfiniteCarousel;
--- components/module-components/learnhub-animation/LearnhubAnimation.tsx
+++ components/module-components/learnhub-animation/LearnhubAnimation.tsx
@@ -4,8 +4,121 @@ import style from './learnhubAnimation.module.scss';
 
 const LearnhubAnimation = () => {
   return (
-    <div className={style.titleController}>
-      <h1 className={style.titleMessage}>LearnHub.mk</h1>
+    <div className={style.slider}>
+      <div className={style.slideTrack}>
+        <div className={style.slide}>
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
+            height="100"
+            width="250"
+            alt=""
+          />
+        </div>
+        <div className={style.slide}>
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png"
+            height="100"
+            width="250"
+            alt=""
+          />
+        </div>
+        <div className={style.slide}>
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png"
+            height="100"
+            width="250"
+            alt=""
+          />
+        </div>
+        <div className={style.slide}>
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png"
+            height="100"
+            width="250"
+            alt=""
+          />
+        </div>
+        <div className={style.slide}>
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png"
+            height="100"
+            width="250"
+            alt=""
+          />
+        </div>
+        <div className={style.slide}>
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png"
+            height="100"
+            width="250"
+            alt=""
+          />
+        </div>
+        <div className={style.slide}>
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png"
+            height="100"
+            width="250"
+            alt=""
+          />
+        </div>
+        <div className={style.slide}>
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
+            height="100"
+            width="250"
+            alt=""
+          />
+        </div>
+        <div className={style.slide}>
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png"
+            height="100"
+            width="250"
+            alt=""
+          />
+        </div>
+        <div className={style.slide}>
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png"
+            height="100"
+            width="250"
+            alt=""
+          />
+        </div>
+        <div className={style.slide}>
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png"
+            height="100"
+            width="250"
+            alt=""
+          />
+        </div>
+        <div className={style.slide}>
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png"
+            height="100"
+            width="250"
+            alt=""
+          />
+        </div>
+        <div className={style.slide}>
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png"
+            height="100"
+            width="250"
+            alt=""
+          />
+        </div>
+        <div className={style.slide}>
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png"
+            height="100"
+            width="250"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 };
