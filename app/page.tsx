fdea8f062537ca7078b38f5739f13a914cd76fcf
Miki Vitkovski <mikivitkovski@gmail.com>
2024-05-10 01:08:50 +0200

    #179 Footer (Create responsive design in footer, update new style from figma, use formik from TextInput for validation, implement toasiify for notification for newsletter, connect links and images for icons,) Delete Newsletter from layout and folder,file with emailform and notification for them,

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@ import Hero from '../components/module-components/hero/Hero';
 import BlogList from '../components/module-components/blog-list/BlogList';
 import LearnhubAnimation from '../components/module-components/learnhub-animation/LearnhubAnimation';
 import styles from './page.module.scss';
-import Newsletter from '../components/reusable-components/newsletter/Newsletter';
 import MemberList from '../components/module-components/member-list/MemberList';
-
 import FAQ_DUMMY_DATA from '../data/FAQ';
 import Contact from '../components/module-components/Contact/Contact';
 
@@ -19,7 +17,6 @@ const Home = () => {
       />
       <LearnhubAnimation />
       <BlogList pageTitle="home" gridLayout="grid__1x3" blogCardsNumber={6} />
-      <Newsletter />
       <MemberList />
       <FAQ data={FAQ_DUMMY_DATA} />
       <Contact />
--- components/reusable-components/footer/Footer.tsx
+++ components/reusable-components/footer/Footer.tsx
@@ -1,69 +1,164 @@
-import React from 'react';
-import Link from 'next/link';
+'use client';
+
+import React, { useState } from 'react';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Image from 'next/image';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
+import Link from 'next/link';
 import styles from './footer.module.scss';
-import EmailForm from '../newsletter/emailform/emailform';
+import TextInput from '../text-input/TextInput';
+import discord from '../../../public/icons-footer/discord.svg';
+import linkedin from '../../../public/icons-footer/linkedIn.svg';
+import github from '../../../public/icons-footer/github.svg';
+import facebook from '../../../public/icons-footer/facebook.svg';
+import instagram from '../../../public/icons-footer/instagram.svg';
+import youtube from '../../../public/icons-footer/youtube.svg';
 
-const Footer = () => {
+const Footer: React.FC = () => {
+  const initialValues = { name: '', email: '' };
+  const validationSchema = Yup.object({
+    name: Yup.string()
+      .matches(/^[a-zA-Z .'-]+$/, 'Name cannot contain special characters!')
+      .min(2, 'Name too Short!')
+      .max(50, 'Name too Long!')
+      .required('Name is required'),
+    email: Yup.string()
+      // eslint-disable-next-line no-unused-vars
+      .email('Invalid email address')
+      .required('Email is required'),
+  });
+  // eslint-disable-next-line no-unused-vars
+  const [successMessage, setSuccessMessage] = useState<boolean>(false);
+  // eslint-disable-next-line no-unused-vars
+  const [errorMessage, setErrorMessage] = useState<boolean>(false);
+
+  // eslint-disable-next-line no-unused-vars
+  const handleSubmit = async (values: { name: string; email: string }) => {
+    try {
+      // Simulating API call
+      /*
+      const response = await fetch('your-backend-api-endpoint', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values),
+      });
+      
+      if (response.ok) {
+        setSuccessMessage(true);
+      } else {
+        setErrorMessage(true);
+      }
+      */
+      // Simulating success
+      setSuccessMessage(true);
+      toast.success('Form submitted successfully!');
+    } catch (error) {
+      setErrorMessage(true);
+      toast.error('Failed to submit form');
+    }
+  };
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: handleSubmit,
+  });
+
   return (
     <footer className={`${styles.footerWrapper}`}>
       <div className={`${styles.footer}`}>
-        <div className={` display-s ${styles.footerLogo}`}>
-          <Image src="/logo/logo.svg" alt="LearnHub Logo" width={250} height={55} />
-        </div>
-
-        <div className={`title-l ${styles.contactSection}`}>
-          <p>CONTACT</p>
-          <a href="mailto:contact@learnhub.mk">contact@learnhub.mk</a>
-        </div>
-
-        <div className={` title-l ${styles.socialMediaSection}`}>
-          <p>SOCIAL MEDIA</p>
-          <Link
-            href="https://www.linkedin.com/company/102600044/admin/feed/posts/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="bi bi-linkedin" /> Linkedin
-          </Link>
-          <Link
-            href="https://www.facebook.com/profile.php?id=61556614894387"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="bi bi-facebook" /> Facebook
-          </Link>
-          <Link
-            href="https://www.instagram.com/learnhub.mk?fbclid=IwAR3bWgALMMaxnp4a9hbUvjXLBOdkWGHcopCsFpp6WyKRRFoMSOyWqVHLNdk_aem_AUc6U-6f2fR5ErfT3g8nroxbZOsFEXjSAh6n06NlfDHWJxvbc-R-DNQ5hJhYnOLCuIWEVpD017qq83l-VqU5y43A"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="bi bi-instagram" /> Instagram
-          </Link>
-          <Link
-            href="https://www.youtube.com/channel/UCDDywMbRvBXuUFTPBORc-BQ"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="bi bi-youtube" /> Youtube
-          </Link>
-          <Link
-            href="https://discord.com/invite/nUEKUWVveW"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="bi bi-discord" /> Discord
-          </Link>
-        </div>
-
-        <div className={` title-l ${styles.newsletterSection}`}>
-          <p>NEWSLETTER</p>
-          <div className={`${styles.inputContainer}`}>
-            <EmailForm inputClassName={styles.newsletterSection} buttonClassName={styles.button} />
+        <form onSubmit={formik.handleSubmit}>
+          <div className={`display-s ${styles.footerLogo}`}>
+            <Image src="/logo/logo.svg" alt="LearnHub Logo" width={208} height={48} />
           </div>
-        </div>
+
+          <div className={`${styles.middleSection}`}>
+            <div className={`title-l ${styles.newsletterSection}`}>
+              <p>Subscribe to our newsletter</p>
+              <div className={`${styles.inputContainer}`}>
+                <TextInput
+                  placeholder="Enter your Name"
+                  type="text"
+                  label=""
+                  name="name"
+                  field="name"
+                  formik={formik}
+                />
+                <TextInput
+                  placeholder="Enter your Email"
+                  type="email"
+                  label=""
+                  name="email"
+                  field="email"
+                  formik={formik}
+                />
+                <button type="submit" className={`${styles.Button}`} disabled={formik.isSubmitting}>
+                  Submit
+                </button>
+              </div>
+            </div>
+
+            <div className={`title-l ${styles.contactSection}`}>
+              <p>Contact us at</p>
+              <a href="mailto:contact@learnhub.mk">contact@learnhub.mk</a>
+            </div>
+
+            <div className={`title-l ${styles.socialMediaSection}`}>
+              <p>Connect with us</p>
+              <div className={styles.socialLinks}>
+                <Link
+                  href="https://discord.com/invite/nUEKUWVveW"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Image src={discord} alt="Discord" width={48} height={48} />
+                </Link>
+                <Link
+                  href="https://www.linkedin.com/company/102600044/admin/feed/posts/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Image src={linkedin} alt="Linkedin" width={48} height={48} />
+                </Link>
+                <Link
+                  href="https://github.com/learnhubmkd"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Image src={github} alt="Github" width={48} height={48} />
+                </Link>
+                <Link
+                  href="https://www.youtube.com/channel/UCDDywMbRvBXuUFTPBORc-BQ"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Image src={youtube} alt="Youtube" width={48} height={48} />
+                </Link>
+                <Link
+                  href="https://www.instagram.com/learnhub.mk?fbclid=IwAR3bWgALMMaxnp4a9hbUvjXLBOdkWGHcopCsFpp6WyKRRFoMSOyWqVHLNdk_aem_AUc6U-6f2fR5ErfT3g8nroxbZOsFEXjSAh6n06NlfDHWJxvbc-R-DNQ5hJhYnOLCuIWEVpD017qq83l-VqU5y43A"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Image src={instagram} alt="Instagram" width={48} height={48} />
+                </Link>
+                <Link
+                  href="https://www.facebook.com/profile.php?id=61556614894387"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Image src={facebook} alt="Facebook" width={48} height={48} />
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className={`title-l ${styles.copyrightSection}`}>
+            <p>&copy; 2024 Copyright by LearnHub. All rights reserved.</p>
+          </div>
+        </form>
       </div>
     </footer>
   );
