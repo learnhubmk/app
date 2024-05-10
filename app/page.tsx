03d3db204fb8b5bf8183d70dba7448b97e61b7fd
kire21 <kire.zdraveski@gmail.com>
2024-05-10 13:49:49 +0200

    fixed errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,16 @@
 import Hero from '../components/module-components/hero/Hero';
 import InfiniteCarousel from '../components/module-components/infinitie-carousel/InfiniteCarousel';
 import PerkCards from '../components/module-components/Perk-Cards/PerkCards';
-import BlogList from '../components/module-components/blog-list/BlogList';
 import Newsletter from '../components/reusable-components/newsletter/Newsletter';
-import MemberList from '../components/module-components/member-list/MemberList';
 import FAQ from '../components/module-components/FAQ/FAQ';
 import Contact from '../components/module-components/Contact/Contact';
 
 import PerkCardsData from '../components/module-components/Perk-Cards/PerkCardsData';
 import FAQ_DUMMY_DATA from '../data/FAQ';
 
-import styles from './page.module.scss';
-
 const Home = () => {
   return (
-    <main className={styles.main}>
+    <main>
       <Hero
         title="Развијте го вашиот потенцијал"
         text="Овозможете си да достигнете нови височини, развивајќи и споделувајќи вештини cо заедница од ентузијасти и професионалци. Со LearnHub, вие не сте само дел  
@@ -22,9 +18,7 @@ const Home = () => {
       />
       <InfiniteCarousel />
       <PerkCards data={PerkCardsData} />
-      <BlogList pageTitle="home" gridLayout="grid__1x3" blogCardsNumber={6} />
       <Newsletter />
-      <MemberList />
       <FAQ data={FAQ_DUMMY_DATA} />
       <Contact />
     </main>
