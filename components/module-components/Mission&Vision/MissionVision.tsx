36c9b2981889a962ad7990a9b70b0b26152f2b9c
Micev <micev@outlook.com>
2024-05-12 23:32:12 +0200

    Added some change in scss

--- components/module-components/Mission&Vision/MissionVision.tsx
+++ components/module-components/Mission&Vision/MissionVision.tsx
@@ -13,7 +13,7 @@ const MissionVision = () => {
     <section
       className={`${style.missionVisionMain} ${isLightTheme ? style.missionMainWhite : style.missionMainDark}`}
     >
-      <div className={style.mvImgContainer} style={{ flex: '0 0 65%' }}>
+      <div className={`${style.mvImgContainer} ${style.mvLeftSide}`}>
         <Image
           className={style.imgMission}
           src="\m&vPhoto.svg"
@@ -22,11 +22,10 @@ const MissionVision = () => {
           height={0}
           sizes="100vw"
           fill
-          style={{ objectFit: 'cover' }}
         />
       </div>
 
-      <div className={style.mvImgContainer} style={{ flex: ' 0 0 35%' }}>
+      <div className={`${style.mvImgContainer} ${style.mvRightSide}`}>
         <Image
           className={style.missionPattern}
           src="/pattern.svg"
@@ -35,16 +34,23 @@ const MissionVision = () => {
           height={0}
           sizes="100vw"
           fill
-          style={{ objectFit: 'cover', paddingBlock: '1.2rem' }}
         />
       </div>
 
       <div
         className={`${style.missionTitle} ${isLightTheme ? style.missionTitleWhite : style.missionTitleDark}`}
       >
-        <div className={style.cardMission} style={{ height: '100%', width: '100%' }}>
+        <div className={style.cardMission}>
+          <Image
+            className={style.mobilePattern}
+            src="./frame6.svg"
+            alt="Mobile Pattern"
+            width={109}
+            height={109}
+          />
+
           <h2>Мисија</h2>
-          <p style={{ height: 'fit-content' }}>
+          <p>
             Мисијата на LearnHub е негување на одржлива и инклузивна заедница од сите припадници на
             IТ-секторот во Македонија, од почетници-ентузијасти до експерти со децениско искуство во
             струката со цел заедничко спојување на вештините и искуствата во насока на тимска
@@ -52,16 +58,15 @@ const MissionVision = () => {
             од идеја до пазар и одржување.
           </p>
 
-          {/* <Image
+          <Image
             className={style.mobilePattern}
             src="./frame6.svg"
             alt="Mobile Pattern"
             width={109}
             height={109}
-          /> */}
-
+          />
           <h2>Визија</h2>
-          <p style={{ height: 'fit-content' }}>
+          <p>
             Визијата на LearnHub е да биде препознатива заедница по соработката, едукацијата и
             поддршката за сите луѓе кои дејствуваат во IT секторот и пошироко. Заедница од каде што
             многу нови производи и услуги ќе бидат резултат на заеднички напори и спој на различни
@@ -69,13 +74,13 @@ const MissionVision = () => {
           </p>
         </div>
 
-        {/* <Image
+        <Image
           className={style.patternHorizontally}
           src="/frame5.svg"
           alt="Mobile Pattern"
           width={67}
           height={104}
-        /> */}
+        />
       </div>
     </section>
   );
