679eaced37dbfb5762661be3f29e0c66f0172646
Dimitar <dimihbt@yahoo.com>
2024-05-20 19:36:30 +0200

    reset api setup

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,25 @@
 import type { Preview } from '@storybook/react';
 import '../app/styles/globals/index.scss';
+import * as NextImage from 'next/image';
+import { setupWorker } from 'msw/browser';
+import { rest } from 'msw/lib/mockServiceWorker';
+const NextImages = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: NextImages,
+});
+
+if (typeof global.process === 'undefined') {
+  const worker = setupWorker(
+    rest.get('http://localhost:3000/api/hello', (req, res, ctx) => {
+      return res(ctx.json({ message: 'Hello from the mock API' }));
+    })
+  );
+
+  worker.start();
+}
+
 const preview: Preview = {
   parameters: {
     controls: {
