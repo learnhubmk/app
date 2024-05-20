1da2b4a2b0918ba84bcb6374aad502b4170a144a
Nino Kadric <ninman27@gmail.com>
2024-05-20 12:36:45 +0200

    #246 fixed navigation style

--- components/module-components/infinitie-carousel/InfiniteCarousel.tsx
+++ components/module-components/infinitie-carousel/InfiniteCarousel.tsx
@@ -1,9 +1,14 @@
+'use client';
+
+import { useTheme } from '../../../app/context/themeContext';
 import multipliedArray from './InfiniteCarouselData';
 import style from './infiniteCarousel.module.scss';
 
 const InfiniteCarousel = () => {
+  const { theme } = useTheme();
+  const isThemeLight = theme === 'light';
   return (
-    <div className={style.slider}>
+    <div className={`${style.slider} ${isThemeLight && style.sliderLight}`}>
       <div className={style.slideTrack}>
         {multipliedArray.map((item) => (
           <div className={style.slide} key={Date.now() + Math.random()}>
