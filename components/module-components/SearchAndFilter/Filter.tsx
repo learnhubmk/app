e8fd9ed7e05516310007258a11224529eb4b2af4
mariocvetanoski <cvetanoski.mario92@gmail.com>
2024-06-05 23:04:06 +0200

    added UserRole enum and removed React.FC

--- components/module-components/SearchAndFilter/Filter.tsx
+++ components/module-components/SearchAndFilter/Filter.tsx
@@ -1,10 +1,15 @@
-// components/Filter.tsx
+/* eslint-disable no-unused-vars */
+
 import React, { useState } from 'react';
 import style from './filter.module.scss';
 
-const options = ['Admin', 'Member', 'Content Manager'];
+export enum UserRole {
+  Admin = 'Admin',
+  Member = 'Member',
+  ContentManager = 'Content Manager',
+}
 
-const Filter: React.FC = () => {
+const Filter = () => {
   const [selectedOption, setSelectedOption] = useState('');
 
   const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -14,10 +19,7 @@ const Filter: React.FC = () => {
   return (
     <div className={style.filter}>
       <select className={style.dropdown} value={selectedOption} onChange={handleChange}>
-        <option value="" disabled hidden>
-          Filter
-        </option>
-        {options.map((option) => (
+        {Object.values(UserRole).map((option) => (
           <option key={option} value={option}>
             {option}
           </option>
