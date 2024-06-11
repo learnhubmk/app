33d0539f188bdfd54c685536dd95223d0a84c9f3
mariocvetanoski <cvetanoski.mario92@gmail.com>
2024-06-11 16:05:24 +0200

    added aria-label to the action-dropdown td

--- components/reusable-components/reusable-table/TableRowComponent.tsx
+++ components/reusable-components/reusable-table/TableRowComponent.tsx
@@ -19,12 +19,16 @@ interface TableRowComponentProps {
 const TableRowComponent = ({ data, onView, onEdit, onDelete }: TableRowComponentProps) => {
   return (
     <tr className={style.rowComponent}>
-      <td onClick={onView} className={style.clickable}>
+      <td
+        onClick={onView}
+        className={style.clickable}
+        aria-label={`View details for ${data.first_name}`}
+      >
         {data.first_name}
       </td>
       <td className={style.lastName}>{data.last_name}</td>
       <td className={style.role}>{data.role}</td>
-      <td className={style.actionCell} aria-label={`${data.first_name}`}>
+      <td className={style.actionCell} aria-label="Actions">
         <ActionDropdown onView={onView} onEdit={onEdit} onDelete={onDelete} />
       </td>
     </tr>
