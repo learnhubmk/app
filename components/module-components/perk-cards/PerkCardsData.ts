e335689459fd963a625d95b846ce0769dabc3916
y0vche <yovche@mail.com>
2024-06-12 10:14:42 +0200

    copy validationn perks section

--- components/module-components/perk-cards/PerkCardsData.ts
+++ components/module-components/perk-cards/PerkCardsData.ts
@@ -1,7 +1,7 @@
 const PerkCardsData = [
   {
     id: 0,
-    perkCardText: 'Меѓусебен раст и усовршување ИТ вештини',
+    perkCardText: 'Меѓусебен развој и усовршување на ИТ вештини',
     perkCardIcon: 'perk-cards-icons/team.svg',
   },
   {
@@ -16,12 +16,12 @@ const PerkCardsData = [
   },
   {
     id: 3,
-    perkCardText: 'Реализирање идеи поттикнати од заедницата',
+    perkCardText: 'Реализирање на идеи поттикнати од заедницата',
     perkCardIcon: 'perk-cards-icons/award.svg',
   },
   {
     id: 4,
-    perkCardText: 'Менторство и конструктивен фидбек',
+    perkCardText: 'Менторство и конструктивен повратен одговор',
     perkCardIcon: 'perk-cards-icons/team.svg',
   },
   {
