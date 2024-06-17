9d23115f03fed044962f9acee590be0b03ba21f6
Dimitar <dimihbt@yahoo.com>
2024-06-17 20:30:48 +0200

    fix

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-links',
@@ -9,13 +10,18 @@ const config: StorybookConfig = {
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
   ],
+
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
-  docs: {
-    autodocs: 'tag',
-  },
+
+  docs: {},
+
   staticDirs: ['../public'],
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 };
 export default config;
---
+++ .storybook/stories/ThemeStatus.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ThemeStatus from './ThemeStatus';
+
+export default {
+  title: 'Theme/ThemeStatus',
+  component: ThemeStatus,
+};
+
+export const Default = () => <ThemeStatus />;
