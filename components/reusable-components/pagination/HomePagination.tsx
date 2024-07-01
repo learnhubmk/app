15c8aad8645866b8e68076eddd4c6c38f063132e
aleksandarmicev <micev@outlook.com>
2024-07-01 23:12:25 +0200

    Removed Structure Pagination because that will be another PR. Here there was a changes in the names in HomePagination, for  UseState (uppercase)

--- components/reusable-components/pagination/HomePagination.tsx
+++ components/reusable-components/pagination/HomePagination.tsx
@@ -1,30 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { fetchItems, Item } from './lib/data';
-import StructurePagination from './StructurePagination';
 import Dropdown from './Dropdown';
 import styles from './HomePagination.module.scss';
 
+/* eslint no-unused-vars: "off" */
 const HomePagination: React.FC = () => {
   const [items, setItems] = useState<Item[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
-  const [itemsPerPage, setItemsPerPage] = useState(25);
+  const [TOTAL_PAGES, SET_TOTAL_PAGES] = useState(1);
+  const [ITEMS_PER_PAGE, SET_ITEMS_PER_PAGE] = useState(25);
 
-  const options = [25, 50, 100];
+  const ITEMS_PER_PAGE_OPTIONS = [25, 50, 100];
 
   useEffect(() => {
     const loadItems = async () => {
-      const data = await fetchItems(currentPage, itemsPerPage);
+      const data = await fetchItems(currentPage, ITEMS_PER_PAGE);
       setItems(data.items);
-      setTotalPages(Math.ceil(data.total / itemsPerPage));
+      SET_TOTAL_PAGES(Math.ceil(data.total / ITEMS_PER_PAGE));
     };
 
     loadItems();
-  }, [currentPage, itemsPerPage]);
+  }, [currentPage, ITEMS_PER_PAGE]);
 
   useEffect(() => {
     setCurrentPage(1);
-  }, [itemsPerPage]);
+  }, [ITEMS_PER_PAGE]);
 
   return (
     <div className={styles.container}>
@@ -37,12 +37,11 @@ const HomePagination: React.FC = () => {
         ))}
       </ul>
       <div className={styles.controls}>
-        <StructurePagination
-          currentPage={currentPage}
-          totalPages={totalPages}
-          onPageChange={setCurrentPage}
+        <Dropdown
+          options={ITEMS_PER_PAGE_OPTIONS}
+          value={ITEMS_PER_PAGE}
+          onChange={SET_ITEMS_PER_PAGE}
         />
-        <Dropdown options={options} value={itemsPerPage} onChange={setItemsPerPage} />
       </div>
     </div>
   );
