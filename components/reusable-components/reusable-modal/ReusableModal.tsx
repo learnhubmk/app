6d93ded8d37927ccca2d926e9d4cda154ffd4ae8
5R <petarkk7@gmail.com>
2024-07-06 20:40:47 +0200

    Refactor ReusableModal
    Refactor button styles and enhance ReusableModal flexibility
    
    Changes:
    - Updated button color classes
    - Removed unnecessary deactivateButton class
    - Refactored secondaryButton class (adjusted colors and font size)
    
    ReusableModal enhancements:
    - Added ability to customize button classNames and labels
    - Implemented custom onClick functions for both buttons
    - Enabled flexible use of buttons as either primary or secondary

--- components/reusable-components/reusable-modal/ReusableModal.tsx
+++ components/reusable-components/reusable-modal/ReusableModal.tsx
@@ -7,9 +7,26 @@ interface ReusableModalProps {
   description?: string;
   isOpen: boolean;
   onClose?: () => void;
+  secondaryButtonLabel?: string;
+  secondaryButtonClass?: string;
+  primaryButtonLabel?: string;
+  primaryButtonClass?: string;
+  onPrimaryButtonClick?: () => void;
+  onSecondaryButtonClick?: () => void;
 }
 
-const ReusableModal = ({ title, description, isOpen, onClose = () => {} }: ReusableModalProps) => {
+const ReusableModal = ({
+  title,
+  description,
+  isOpen,
+  secondaryButtonLabel = 'Cancel',
+  secondaryButtonClass = '',
+  onPrimaryButtonClick,
+  onSecondaryButtonClick,
+  primaryButtonLabel = 'Proceed',
+  primaryButtonClass = '',
+  onClose = () => {},
+}: ReusableModalProps) => {
   useEffect(() => {
     if (isOpen) {
       const scrollBarWidth = window.innerWidth - document.documentElement.clientWidth;
@@ -67,15 +84,15 @@ const ReusableModal = ({ title, description, isOpen, onClose = () => {} }: Reusa
           <div className={style.modalButtons}>
             <Button
               type="button"
-              buttonText="Cancel"
-              buttonClass={['primaryButton']}
-              onClick={onClose}
+              buttonText={primaryButtonLabel || 'Proceed'}
+              buttonClass={primaryButtonClass ? [primaryButtonClass] : ['primaryButton']}
+              onClick={onPrimaryButtonClick || onClose}
             />
             <Button
               type="button"
-              buttonText="Deactivate"
-              buttonClass={['secondaryButton', 'deactivateButton']}
-              onClick={onClose}
+              buttonText={secondaryButtonLabel || 'Cancel'}
+              buttonClass={secondaryButtonClass ? [secondaryButtonClass] : ['secondaryButton']}
+              onClick={onSecondaryButtonClick || onClose}
             />
           </div>
         </div>
