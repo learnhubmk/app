73ed7fbe07650ab611617ffc8ac695308674f81e
petark7 <petarkk7@gmail.com>
2024-07-15 15:30:35 +0200

    Update page.tsx

--- app/content-panel/tags/page.tsx
+++ app/content-panel/tags/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React, { useState } from 'react';
-import { ToastContainer } from 'react-toastify';
 import styles from './Tags.module.scss';
 import TagManagementControls from './TagManagementControls';
 import TagTable from './TagTable';
