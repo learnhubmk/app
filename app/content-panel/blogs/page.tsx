8d10bb5a00956773ed3c1aebf0cbe46d4c90bdf2
aleksandarmicev <micev@outlook.com>
2024-12-17 00:17:44 +0100

    Removed component for testing

--- app/content-panel/blogs/page.tsx
+++ app/content-panel/blogs/page.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import BlogListView from '../../../components/module-components/create-blogs/BlogListView';
-import PostArticle from './create/page';
 
 const page = () => {
-  return (
-    <div>
-      <BlogListView />
-      <PostArticle />
-    </div>
-  );
+  return <BlogListView />;
 };
 
 export default page;
